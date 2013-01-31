93b39fa82eff31ea257b0e817a29519fe6ab80ce
btmills <mills.brandont@gmail.com>
2013-01-31 12:31:01 -0500

    Clean up editor init

--- assets/js/tracer.js
+++ assets/js/tracer.js
@@ -1,24 +1,3 @@
-/*function parse() {
-	return esprima.parse($('#code').val());
-}
-
-var editor;
-
-$(document).ready(function () {
-
-	editor = ace.edit('editor');
-	//editor.setTheme('ace/theme/monokai');
-	//editor.getSession().setMode('ace/mode/javascript');
-
-});*/
-
-//var editor = ace.edit('editor');
-
-//var code;
-//var output;
-
-//$(document).ready(function () {
-
 var editor = ace.edit('code');
 editor.setTheme('ace/theme/monokai');
 editor.getSession().setMode('ace/mode/javascript');
@@ -36,5 +15,3 @@ output.getSession().setMode('ace/mode/json');
 output.setReadOnly(true);
 output.setHighlightActiveLine(false);
 output.setShowPrintMargin(false);
-
-//});
